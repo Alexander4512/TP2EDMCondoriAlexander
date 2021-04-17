3889bfc20ab2fae16727c975efaf363789831fc5
Alex <Alex@Alex>
2021-04-16 21:47:52 -0300

    inicializacion de proyecto Alexander Condori

---
+++ src/main/java/ar/edu/unju/edm/Tp2CondoriEdm2021Application.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2CondoriEdm2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2CondoriEdm2021Application.class, args);
+	}
+
+}
+
+
+
+
